dc10f343eda0500555a89c347f486295c47309d8
Lourenzo Ferreira <lourenzo@gmail.com>
2023-11-09 20:27:41 -0300

    chore: add script to download non-commercial datasets

---
+++ datasets/download.sh
@@ -0,0 +1,7 @@
+curl https://datasets.imdbws.com/name.basics.tsv.gz -o name.basics.tsv.gz
+curl https://datasets.imdbws.com/title.akas.tsv.gz -o title.akas.tsv.gz
+curl https://datasets.imdbws.com/title.basics.tsv.gz -o title.basics.tsv.gz
+curl https://datasets.imdbws.com/title.crew.tsv.gz -o title.crew.tsv.gz
+curl https://datasets.imdbws.com/title.episode.tsv.gz -o title.episode.tsv.gz
+curl https://datasets.imdbws.com/title.principals.tsv.gz -o title.principals.tsv.gz
+curl https://datasets.imdbws.com/title.ratings.tsv.gz -o title.ratings.tsv.gz
