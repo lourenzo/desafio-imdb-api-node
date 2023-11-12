e0c16fd2e86df521de23d09692af3b3687b6327f
Lourenzo Ferreira <lourenzo@gmail.com>
2023-11-11 22:46:28 -0300

    feat: final stream processor to gather possible categories
    Note: adding type with all possible found categories

---
+++ datasets/PrincipalCategory.ts
@@ -0,0 +1,13 @@
+type PrincipalCategory =
+  | "self"
+  | "director"
+  | "cinematographer"
+  | "composer"
+  | "producer"
+  | "editor"
+  | "actor"
+  | "actress"
+  | "writer"
+  | "production_designer"
+  | "archive_footage"
+  | "archive_sound";
--- datasets/distinct-principal-category.ts
+++ datasets/distinct-principal-category.ts
@@ -4,6 +4,7 @@ import { parse } from "csv";
 
 const parseFile = (fileName: string) => {
   console.log("Starting stream processing...");
+  
   const reader = createReadStream(fileName);
   const gunzip = createGunzip();
   const parser = parse({
@@ -11,11 +12,22 @@ const parseFile = (fileName: string) => {
     relax_quotes: true,
     columns: true,
   });
+
   const categories = new Set();
+  
+  let lineCount = 0;
+  const totalLines = 58969177;
+  
   parser.on("readable", function () {
     let record: any;
     while ((record = parser.read()) !== null) {
       categories.add(record.category);
+      lineCount ++;
+
+      if (lineCount%100 == 0) {
+        const rate = lineCount * 100 / totalLines;
+        process.stdout.write(`\rLine: ${lineCount} of ${totalLines} -> ${rate.toLocaleString()}%`);
+      }
     }
   });
 
