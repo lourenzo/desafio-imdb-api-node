3ba0f80badc4e1e18205040513d60f1e78e793bd
Lourenzo Ferreira <lourenzo@gmail.com>
2023-11-11 22:23:37 -0300

    feat: extract principal possiblie categories

--- datasets/distinct-principal-category.ts
+++ datasets/distinct-principal-category.ts
@@ -1,27 +1,29 @@
-import {createReadStream, createWriteStream} from 'fs';
-import {createGunzip} from 'zlib';
-import {parse} from 'csv';
+import { createReadStream, createWriteStream } from "fs";
+import { createGunzip } from "zlib";
+import { parse } from "csv";
 
 const parseFile = (fileName: string) => {
-    console.log('oi');
-    const reader = createReadStream(fileName);
-    const gunzip = createGunzip();
-    const parser = parse({
-        delimiter: "\t",
-        relax_quotes: true,
-        escape: '\\', ltrim: true, rtrim: true
-    });
-    const records: any[] = [];
-    parser.on('readable', function(){
-        let record: any;
-        while ((record = parser.read()) !== null) {
-          records.push(record);
-        }
-      })
-    
-    reader.pipe(gunzip).pipe(parse())
+  console.log("Starting stream processing...");
+  const reader = createReadStream(fileName);
+  const gunzip = createGunzip();
+  const parser = parse({
+    delimiter: "\t",
+    relax_quotes: true,
+    columns: true,
+  });
+  const categories = new Set();
+  parser.on("readable", function () {
+    let record: any;
+    while ((record = parser.read()) !== null) {
+      categories.add(record.category);
+    }
+  });
 
-    
-}
+  reader.pipe(gunzip).pipe(parser);
+
+  parser.on("end", () => {
+    console.log(categories);
+  });
+};
 
 parseFile("./title.principals.tsv.gz");
